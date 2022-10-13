4a62797706f987cd302a12e396c1cd76f4a98343
khoonidewana <71880944+khoonidewana@users.noreply.github.com>
2022-10-13 18:50:38 +0530

    Add files via upload

---
+++ src/locatorinselenium/locatorinselenium.java
@@ -0,0 +1,22 @@
+package locatorinselenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class locatorinselenium {
+	@Test
+public void IDDemo() {
+	System.setProperty("webdriver.chrome.driver"
+			,"D:\\ECLIPSE dxc phase 2\\DXCLaunchChrome\\SeleniumChromeBrowserJars\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://opensource-demo.orangehrmlive.com/");
+	driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+	driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+	driver.findElement(By.id("btnLogin")).click();
+	
+	
+}
+}
